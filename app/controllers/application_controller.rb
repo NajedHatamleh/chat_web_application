c2f6abfd279da30e1cdaab96163cbe50b3b89857
Nejed Hatamleh <najedhatamleh77@gmail.com>
2019-09-03 14:02:30 +0300

    Creating Room functionalty, Minimal usage of Bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:signup, keys: [:email, :username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username])
+    # debugger
   end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,6 +5,38 @@ class RoomsController < ApplicationController
   end
 
   def index
-      
+    @rooms = Room.all
   end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new permitted_parameters
+    debugger
+    if @room.save
+      flash[:success] = "Room #{@room.name} wass created successfully"
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @room.update_attributes(permitted_parameters)
+      flash[:success] = "Room #{@room.name} has been updated successfully"
+      redirect_to rooms_path
+    else
+      flash[:error_messages] = "Room wasn't updated"
+      render :new
+    end
+  end
+
+  protected
+
+  def permitted_parameters
+    params.require(:room).permit(:name)
+  end
+
 end
